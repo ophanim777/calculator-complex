122706d6240d481b2b7bf1a91c35a6c8c48a199a
ophanim777 <rikyjo49@gmail.com>
2025-11-09 07:05:24 +0700

    add display function

--- javascript.js
+++ javascript.js
@@ -9,3 +9,10 @@ function subtract(a, b) {
 function multiply(a, b) {
   return a * b;
 }
+
+function divide(a, b) {
+  if (b === 0) {
+    return "Error: Can't divide by 0!";
+  }
+  return a / b;
+}
