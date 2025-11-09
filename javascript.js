5f93bdd2fdbc4355b5cd0c02918273c90a923328
ophanim777 <rikyjo49@gmail.com>
2025-11-09 07:06:28 +0700

    add operation function

--- javascript.js
+++ javascript.js
@@ -16,3 +16,21 @@ function divide(a, b) {
   }
   return a / b;
 }
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      return null;
+  }
+}
